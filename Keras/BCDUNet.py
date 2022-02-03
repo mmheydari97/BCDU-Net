111c2683a4425e5a17b0518283be3cd5ecff0d08
mmheydari97 <heidarymm@yahoo.com>
2022-02-03 16:19:42 -0500

    initial commit

---
+++ Keras/BCDUNet.py
@@ -0,0 +1,83 @@
+import numpy as np
+from tensorflow.keras.layers import Input, concatenate, Conv2D, Conv2DTranspose, Activation, ConvLSTM2D
+from tensorflow.keras.layers import MaxPooling2D, Reshape, Dropout, BatchNormalization, Bidirectional
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam, SGD
+
+
+def BCDUNet(input_size = (256,256,3), output_c=3):
+    N = input_size[0]
+    inputs = Input(input_size)
+    # comments show corresponding layers in pytorch implementation.
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs) #conv1_0
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1) #conv1_1
+
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1) #conv2_0
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2) #conv2_0
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2) #conv3_0
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3) #conv3_1
+    drop3 = Dropout(0.5)(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # D1
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3) #conv4_0
+    conv4_1 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4) #conv4_1
+    drop4_1 = Dropout(0.5)(conv4_1)
+    # D2
+    conv4_2 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(drop4_1) #conv4_2
+    conv4_2 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_2) #conv4_3
+    conv4_2 = Dropout(0.5)(conv4_2)
+    # D3
+    merge_dense = concatenate([conv4_2,drop4_1], axis = 3)
+   
+    conv4_3 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge_dense) #conv4_4
+    conv4_3 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_3) #conv4_5
+    drop4_3 = Dropout(0.5)(conv4_3)
+
+    up6 = Conv2DTranspose(256, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_3) #convt1
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(drop3)
+    x2 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(up6)
+    merge6  = concatenate([x1,x2], axis = 1)
+    
+    merge6 = Bidirectional(ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, kernel_initializer = 'he_normal'), merge_mode="concat")(merge6) #clstm1
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6) #conv6_0
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6) #conv6_1
+
+    up7 = Conv2DTranspose(128, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv6) #convt2
+    up7 = BatchNormalization(axis=3)(up7) #bn2
+    up7 = Activation('relu')(up7)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
+    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up7)
+    merge7  = concatenate([x1,x2], axis = 1)
+    merge7 = Bidirectional(ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, kernel_initializer = 'he_normal'), merge_mode="concat")(merge7) #clstm2
+
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7) #conv7_0
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7) #conv7_1
+
+    up8 = Conv2DTranspose(64, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv7) #convt3
+    up8 = BatchNormalization(axis=3)(up8) #bn3
+    up8 = Activation('relu')(up8)
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up8)
+    merge8  = concatenate([x1,x2], axis = 1)
+    merge8 = Bidirectional(ConvLSTM2D(filters = 16, kernel_size=(3, 3), padding='same', return_sequences = False,kernel_initializer = 'he_normal' ), merge_mode="concat")(merge8) #clstm3
+
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8) #conv8_0
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8) #conv8_1
+    conv8 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8) #conv8_2
+
+    conv9 = Conv2D(output_c, 1, activation = 'sigmoid')(conv8) #conv9_0
+
+    model = Model(inputs = inputs, outputs = conv9)
+    model.compile(optimizer = Adam(learning_rate=1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+if __name__ == "__main__":
+    bcd = BCDUNet()
+    print(bcd.summary())
---
+++ Torch/BCDUNet.py
@@ -0,0 +1,126 @@
+import torch 
+import torch.nn as nn
+from BCDUNet import ConvBLSTM
+
+class BCDUNet(nn.Module):
+    def __init__(self, input_dim=3, output_dim=3, num_filter=64, norm='instance'):
+        super(BCDUNet, self).__init__()
+        self.num_filter = num_filter
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2).cuda()
+        self.dropout = nn.Dropout(0.5).cuda()
+
+        self.conv1_0 = nn.Conv2d(input_dim, num_filter, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv1_1 = nn.Conv2d(num_filter, num_filter, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv2_0 = nn.Conv2d(num_filter, num_filter*2, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv2_1 = nn.Conv2d(num_filter*2, num_filter*2, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv3_0 = nn.Conv2d(num_filter*2, num_filter*4, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv3_1 = nn.Conv2d(num_filter*4, num_filter*4, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv4_0 = nn.Conv2d(num_filter*4, num_filter*8, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv4_1 = nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv4_2 = nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv4_3 = nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv4_4 = nn.Conv2d(num_filter*16, num_filter*8, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv4_5 = nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1).cuda()
+        
+        self.conv6_0 = nn.Conv2d(num_filter*2, num_filter*4, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv6_1 = nn.Conv2d(num_filter*4, num_filter*4, kernel_size=3, stride=1, padding=1).cuda()
+
+        self.conv7_0 = nn.Conv2d(num_filter, num_filter*2, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv7_1 = nn.Conv2d(num_filter*2, num_filter*2, kernel_size=3, stride=1, padding=1).cuda()
+
+        self.conv8_0 = nn.Conv2d(num_filter//2, num_filter, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv8_1 = nn.Conv2d(num_filter, num_filter, kernel_size=3, stride=1, padding=1).cuda()
+        self.conv8_2 = nn.Conv2d(num_filter, num_filter//2, kernel_size=3, stride=1, padding=1).cuda()
+
+        self.conv9_0 = nn.Conv2d(num_filter//2, output_dim, kernel_size=1, stride=1).cuda()
+
+        self.convt1 = nn.ConvTranspose2d(num_filter*8, num_filter*4, kernel_size=2, stride=2, padding=0).cuda()
+        self.bn1 = nn.BatchNorm2d(num_filter*4).cuda()
+        self.convt2 = nn.ConvTranspose2d(num_filter*4, num_filter*2, kernel_size=2, stride=2, padding=0).cuda()
+        self.bn2 = nn.BatchNorm2d(num_filter*2).cuda()
+        self.convt3 = nn.ConvTranspose2d(num_filter*2, num_filter, kernel_size=2, stride=2, padding=0).cuda()
+        self.bn3 = nn.BatchNorm2d(num_filter).cuda()
+
+
+        self.clstm1 = ConvBLSTM(in_channels=num_filter*4, hidden_channels=num_filter, kernel_size=(3, 3), num_layers=1).cuda()
+        self.clstm2 = ConvBLSTM(in_channels=num_filter*2, hidden_channels=num_filter//2, kernel_size=(3, 3), num_layers=1).cuda()
+        self.clstm3 = ConvBLSTM(in_channels=num_filter, hidden_channels=num_filter//4, kernel_size=(3, 3), num_layers=1).cuda()
+        
+        
+    def forward(self, x):
+        N = x.size()[-2]
+        conv1 = self.conv1_0(x)
+        conv1 = self.conv1_1(conv1)
+        pool1 = self.maxpool(conv1)
+        conv2 = self.conv2_0(pool1)
+        conv2 = self.conv2_1(conv2)
+        pool2 = self.maxpool(conv2)
+        conv3 = self.conv3_0(pool2)
+        conv3 = self.conv3_1(conv3)
+        drop3 = self.dropout(conv3)
+        pool3 = self.maxpool(conv3)
+        # D1
+        conv4 = self.conv4_0(pool3)
+        conv4_1 = self.conv4_1(conv4)
+        drop4_1 = self.dropout(conv4_1)
+        # D2
+        conv4_2 = self.conv4_2(drop4_1)
+        conv4_2 = self.conv4_3(conv4_2)
+        conv4_2 = self.dropout(conv4_2)
+        # D3
+        merge_dense = torch.cat((conv4_2, drop4_1), 1)
+        conv4_3 = self.conv4_4(merge_dense)
+        conv4_3 = self.conv4_5(conv4_3)
+        drop4_3 = self.dropout(conv4_3)
+ 
+        up6 = self.convt1(drop4_3)
+        up6 = self.bn1(up6)
+        up6 = nn.ReLU()(up6)
+
+        x1 = drop3.view(-1,1,self.num_filter*4,N//4,N//4)
+        x2 = up6.view(-1,1,self.num_filter*4,N//4,N//4)
+
+        merge6 = torch.cat((x1, x2), 1)
+        merge6 = self.clstm1(merge6)
+        
+        conv6 = self.conv6_0(merge6)
+        conv6 = self.conv6_1(conv6)
+
+        up7 = self.convt2(conv6)
+        up7 = self.bn2(up7)
+        up7 = nn.ReLU()(up7)
+
+        x1 = conv2.view(-1,1,self.num_filter*2,N//2,N//2)
+        x2 = up7.view(-1,1,self.num_filter*2,N//2,N//2)
+        merge7 = torch.cat((x1, x2), 1)
+        merge7 = self.clstm2(merge7)
+
+        conv7 = self.conv7_0(merge7)
+        conv7 = self.conv7_1(conv7)
+
+        up8 = self.convt3(conv7)
+        up8 = self.bn3(up8)
+        up8 = nn.ReLU()(up8)
+
+        x1 = conv1.view(-1,1,self.num_filter,N,N)
+        x2 = up8.view(-1,1,self.num_filter,N,N)
+        merge8 = torch.cat((x1, x2), 1)
+        merge8 = self.clstm3(merge8)
+
+        conv8 = self.conv8_0(merge8)
+        conv8 = self.conv8_1(conv8)
+        conv8 = self.conv8_2(conv8)
+
+        conv9 = self.conv9_0(conv8)
+
+        return conv9
+
+
+if __name__ == '__main__':
+
+    x = torch.randn([8, 3, 256, 256]).cuda()
+
+    bcdunet = BCDUNet().cuda()
+    out = bcdunet(x)
+    print (out.shape)
+    out.sum().backward()
