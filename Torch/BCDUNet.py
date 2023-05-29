776a7787aede34e07e64ee316c50113ac41a4daa
mmehydari97 <heidarymm@yahoo.com>
2023-05-28 23:05:05 -0400

    add norm to BCDUNet

--- Torch/BCDUNet.py
+++ Torch/BCDUNet.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 from ConvLSTM import ConvBLSTM, ConvLSTM
 
+
 class BCDUNet(nn.Module):
     def __init__(self, input_dim=3, output_dim=3, num_filter=64, frame_size=(256, 256), bidirectional=False, norm='instance'):
         super(BCDUNet, self).__init__()
@@ -11,68 +12,35 @@ class BCDUNet(nn.Module):
         self.dropout = nn.Dropout(0.5)
         self.frame_size = np.array(frame_size)
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(input_dim, num_filter, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter, num_filter, kernel_size=3, stride=1, padding=1)
-        )
+        if norm == 'instance':
+            norm_layer = nn.InstanceNorm2d
+        else:
+            norm_layer = nn.BatchNorm2d
 
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(num_filter, num_filter*2, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*2, num_filter*2, kernel_size=3, stride=1, padding=1)
-        )
+        def conv_block(in_channels, out_channels):
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+                norm_layer(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+                norm_layer(out_channels),
+                nn.ReLU(inplace=True)
+            )
 
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(num_filter*2, num_filter*4, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*4, num_filter*4, kernel_size=3, stride=1, padding=1)
-        )
+        self.conv1 = conv_block(input_dim, num_filter)
+        self.conv2 = conv_block(num_filter, num_filter * 2)
+        self.conv3 = conv_block(num_filter * 2, num_filter * 4)
+        self.conv4 = conv_block(num_filter * 4, num_filter * 8)
 
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(num_filter*4, num_filter*8, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1),
-            nn.Dropout(0.5),
-            nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1),
-            nn.Dropout(0.5),
-            nn.Conv2d(num_filter*16, num_filter*8, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*8, num_filter*8, kernel_size=3, stride=1, padding=1),
-            nn.Dropout(0.5)
-        )
+        self.upconv3 = nn.ConvTranspose2d(num_filter * 8, num_filter * 4, kernel_size=2, stride=2)
+        self.upconv2 = nn.ConvTranspose2d(num_filter * 4, num_filter * 2, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(num_filter * 2, num_filter, kernel_size=2, stride=2)
 
-        self.conv6 = nn.Sequential(
-            nn.Conv2d(num_filter*2, num_filter*4, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*4, num_filter*4, kernel_size=3, stride=1, padding=1)
-        )
+        self.conv3m = conv_block(num_filter * 8, num_filter * 4)
+        self.conv2m = conv_block(num_filter * 4, num_filter * 2)
+        self.conv1m = conv_block(num_filter * 2, num_filter)
 
-        self.conv7 = nn.Sequential(
-            nn.Conv2d(num_filter, num_filter*2, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter*2, num_filter*2, kernel_size=3, stride=1, padding=1)
-        )
-
-        self.conv8 = nn.Sequential(
-            nn.Conv2d(num_filter//2, num_filter, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter, num_filter, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(num_filter, num_filter//2, kernel_size=3, stride=1, padding=1)
-        )
-
-        self.conv9 = nn.Conv2d(num_filter//2, output_dim, kernel_size=1, stride=1)
-
-        self.convt1 = nn.Sequential(
-            nn.ConvTranspose2d(num_filter*8, num_filter*4, kernel_size=2, stride=2, padding=0),
-            nn.BatchNorm2d(num_filter*4),
-            nn.ReLU(inplace=True)
-        )
-
-        self.convt2 = nn.Sequential(
-            nn.ConvTranspose2d(num_filter*4, num_filter*2, kernel_size=2, stride=2, padding=0),
-            nn.BatchNorm2d(num_filter*2),
-            nn.ReLU(inplace=True)
-        )
-
-        self.convt3 = nn.Sequential(
-            nn.ConvTranspose2d(num_filter*2, num_filter, kernel_size=2, stride=2, padding=0),
-            nn.BatchNorm2d(num_filter),
-            nn.ReLU(inplace=True)
-        )
+        self.conv0 = nn.Conv2d(num_filter, output_dim, kernel_size=1)
 
         if bidirectional:
             self.clstm1 = ConvBLSTM(num_filter*4, num_filter*2, (3, 3), (1,1), 'tanh', list(self.frame_size//4))
@@ -82,7 +50,7 @@ class BCDUNet(nn.Module):
             self.clstm1 = ConvLSTM(num_filter*4, num_filter*2, (3, 3), (1,1), 'tanh', list(self.frame_size//4))
             self.clstm2 = ConvLSTM(num_filter*2, num_filter, (3, 3), (1,1), 'tanh', list(self.frame_size//2))
             self.clstm3 = ConvLSTM(num_filter, num_filter//2, (3, 3), (1,1), 'tanh', list(self.frame_size))
-        
+
     def forward(self, x):
         N = self.frame_size
         conv1 = self.conv1(x)
@@ -90,30 +58,21 @@ class BCDUNet(nn.Module):
         conv2 = self.conv2(pool1)
         pool2 = self.maxpool(conv2)
         conv3 = self.conv3(pool2)
-        drop3 = self.dropout(conv3)
         pool3 = self.maxpool(conv3)
-        drop4_3 = self.conv4(pool3)
+        conv4 = self.conv4(pool3)
 
-        up6 = self.convt1(drop4_3)
-        x1 = drop3.view(-1,1,self.num_filter*4,*(N//4))
-        x2 = up6.view(-1,1,self.num_filter*4,*(N//4))
-        merge6 = torch.cat((x1, x2), 1)
-        merge6 = self.clstm1(merge6)
-        conv6 = self.conv6(merge6)
+        upconv3 = self.upconv3(conv4)
+        concat3 = torch.cat((conv3, upconv3), 1)
+        conv3m = self.conv3m(concat3)
 
-        up7 = self.convt2(conv6)
-        x1 = conv2.view(-1,1,self.num_filter*2,*(N//2))
-        x2 = up7.view(-1,1,self.num_filter*2,*(N//2))
-        merge7 = torch.cat((x1, x2), 1)
-        merge7 = self.clstm2(merge7)
-        conv7 = self.conv7(merge7)
+        upconv2 = self.upconv2(conv3m)
+        concat2 = torch.cat((conv2, upconv2), 1)
+        conv2m = self.conv2m(concat2)
 
-        up8 = self.convt3(conv7)
-        x1 = conv1.view(-1,1,self.num_filter,*N)
-        x2 = up8.view(-1,1,self.num_filter,*N)
-        merge8 = torch.cat((x1, x2), 1)
-        merge8 = self.clstm3(merge8)
-        conv8 = self.conv8(merge8)
-        conv9 = self.conv9(conv8)
+        upconv1 = self.upconv1(conv2m)
+        concat1 = torch.cat((conv1, upconv1), 1)
+        conv1m = self.conv1m(concat1)
 
-        return conv9
+        conv0 = self.conv0(conv1m)
+
+        return conv0
